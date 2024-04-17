c68e94166d65ab6a01c249b1a6e967cbd5181c90
Primi373-creator <120999508+Primi373-creator@users.noreply.github.com>
2024-04-17 17:17:06 +0100

    Update session.js

--- model/session.js
+++ model/session.js
@@ -3,6 +3,9 @@ const mongoose = require('mongoose');
 const PasteSchema = new mongoose.Schema({
   id: String,
   content: String,
+  createdAt: { type: Date, default: Date.now }
 });
+
+PasteSchema.index({ createdAt: 1 }, { expireAfterSeconds: 604800 });
 
 module.exports = mongoose.model('Paste', PasteSchema);
